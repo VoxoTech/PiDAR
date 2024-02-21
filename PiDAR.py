62549f02aee0d4974ed4ae0f37eb5a13da4da49e
LaserBorg <mail@philipgutjahr.de>
2024-02-21 21:32:25 +0100

    A4988 integration

--- PiDAR.py
+++ PiDAR.py
@@ -12,53 +12,55 @@ from lib.file_utils import save_data, make_dir
 # allow access to serial port on Raspberry Pi
 allow_serial()
 
-# CONSTANTS
-PORT = '/dev/ttyS0'         # {'Windows': 'COM10', 'RaspberryPi': '/dev/ttyS0', 'Linux': '/dev/ttyUSB0'}  # dmesg | grep "tty"
-PWM_DC = 0.2                # duty cycle: 0.23 = 10 Hz, 0.1 = 4 Hz
-OFFSET = np.pi / 2          # = 90°
-FORMAT = 'npy'              # 'npy' or 'csv' or None
-DTYPE = np.float64          # np.float32
-DATA_DIR = "data"
-VIS = plot_2D()             # plot_2D() or None
-OUT_LEN = 40                # visualize after every nth batch
-
-DIR_PIN = 26                # direction pin
-STEP_PIN = 19               # step pin
-MS_PINS = [5, 6, 13]        # microstepping mode pins
-
-STEP_DELAY = 0.001         # seconds between steps
-STEP_ANGLE = 1.8            # degrees per full step
-MICROSTEPS = 16             # microsteps per full step
-GEAR_RATIO = 3.7142857      # planetary gear reduction ratio
-
-HORIZONTAL_ANGLE = 0.4846444  # degrees
-HORIZONTAL_STEPS = 16
-
-
-GPIO.setwarnings(False)  # Disable warnings
+# GPIO SETUP
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT)
+GPIO.setwarnings(False)
 
-# Relay Power on
-GPIO.output(24, 1)
+# LiDAR DRIVER
+PORT = '/dev/ttyS0'                         # {'Windows': 'COM10', 'RaspberryPi': '/dev/ttyS0', 'Linux': '/dev/ttyUSB0'}  # dmesg | grep "tty"
+PWM_DC = 0.2                                # duty cycle: 0.23 = 10 Hz, 0.1 = 4 Hz
+OFFSET = np.pi / 2                          # = 90°
+FORMAT = 'npy'                              # 'npy' or 'csv' or None
+DTYPE = np.float32                          # np.float64 or np.float32
+DATA_DIR = "data"
+VIS = plot_2D()                             # plot_2D() or None
+OUT_LEN = 40                                # visualize after every nth batch
+
+# A4988 DRIVER
+DIR_PIN = 26
+STEP_PIN = 19
+MS_PINS = [5, 6, 13]
+MICROSTEPS = 16                             # microstepping mode
+MS_TABLE = {16: 11885, 8: 5942, 4: 2971}    # table of microsteps per revolution
+MS360 = MS_TABLE[MICROSTEPS]                # microsteps per revolution
+TARGET_RES = 0.5                            # desired resolution in degrees
+STEP_DELAY = 0.0005                         # seconds between steps
+GEAR_RATIO = 3.7142857                      # planetary gear reduction ratio
+STEP_ANGLE = 1.8                            # degrees per full step (360 / 200)
+
+steps = int(MS360 * TARGET_RES / 360)       # 16
+h_res = 360 * steps / MS360                 # 0.48464451
+scan_delay = 1 / (4500 * TARGET_RES / 360)  # 0.16
+
+# Power Relay
+RELAY_PIN = 24
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.output(RELAY_PIN, 1)                   # Relay Power on
 
 # ensure output directory
 make_dir(DATA_DIR)
 
-
+# initialize devices
 lidar   = LD06(port=PORT, pwm_dc = PWM_DC, visualization=VIS, offset=OFFSET,format=FORMAT, dtype=DTYPE, data_dir=DATA_DIR, out_len=OUT_LEN)
 stepper = A4988(DIR_PIN, STEP_PIN, MS_PINS, delay=STEP_DELAY, step_angle=STEP_ANGLE, microsteps=MICROSTEPS, gear_ratio=GEAR_RATIO)
 
 
-## calculate exact amount of steps for the desired angle
-#steps = stepper.move_angle(HORIZONTAL_ANGLE)
-#print(f"[INFO] {HORIZONTAL_ANGLE}° -> {steps} steps.")
-
-
+# MAIN
 try:
-    for z_angle in np.arange(0, 360, HORIZONTAL_ANGLE):
+    # 0-180° SCAN
+    for z_angle in np.arange(0, 180, h_res):
+
         if lidar.serial_connection.is_open:
-            # lidar.read_loop()  
         
             if lidar.out_i == lidar.out_len:
                 # print("speed:", round(lidar.speed, 2))
@@ -72,15 +74,24 @@ try:
                     lidar.visualization.update_coordinates(lidar.points_2d)
                 lidar.out_i = 0
 
+            # READ LIDAR DATA FROM SERIAL
             lidar.read()
             lidar.out_i += 1
 
-            stepper.move_steps(HORIZONTAL_STEPS)
+            stepper.move_steps(steps)
+            sleep(scan_delay)                   # duration of one lidar duration
+        stepper.move_steps(steps)               # n+1 step
+
+        sleep(1)
+        
+        # 180-360° JUST SHOOTING PHOTOS
+        for i in range(4):
+            stepper.move_angle(45)
+            sleep(1)                            # delay for photo
 
 finally:
     print("SCANNING STOPPED")
-    GPIO.output(24, 0)  # Relay Power off
+    GPIO.output(24, 0)                          # Relay Power off
     GPIO.cleanup()
-
     lidar.close()
     stepper.close()
--- lib/a4988_driver.py
+++ lib/a4988_driver.py
@@ -7,7 +7,7 @@ https://i.stack.imgur.com/vN7JL.png
 '''
 
 import RPi.GPIO as GPIO     # type: ignore
-import time
+from time import sleep
 
 class A4988:
     def __init__(self, dir_pin, step_pin, ms_pins, delay=0.0001, step_angle=1.8, microsteps=16, gear_ratio=1.0, verbose=False):
@@ -49,9 +49,9 @@ class A4988:
 
     def step(self):
         GPIO.output(self.step_pin, True)
-        time.sleep(0.0001)
+        sleep(0.0001)
         GPIO.output(self.step_pin, False)
-        time.sleep(self.delay)
+        sleep(self.delay)
 
     def move_steps(self, steps):
         direction = steps < 0   # < clockwise positive
@@ -76,43 +76,43 @@ class A4988:
 if __name__ == "__main__":
     import numpy as np
 
-    dir_pin = 26
-    step_pin = 19
-    ms_pins = [5, 6, 13]
+    DIR_PIN = 26
+    STEP_PIN = 19
+    MS_PINS = [5, 6, 13]
 
-    ms = 16                                     # microstepping mode
-    ms_table = {16: 11885, 8: 5942, 4: 2971}    # table of microsteps per revolution
-    ms360 = ms_table[ms]                        # microsteps per revolution
+    MICROSTEPS = 16                             # microstepping mode
+    MS_TABLE = {16: 11885, 8: 5942, 4: 2971}    # table of microsteps per revolution
+    MS360 = MS_TABLE[MICROSTEPS]                # microsteps per revolution
 
-    target_res = 0.5                            # desired resolution in degrees
-    delay = 0.0005
-    gear_ratio = 3.7142857
-    stepper_resolution = 200
-    step_angle = 360 / stepper_resolution       # 1.8
+    TARGET_RES = 0.5                            # desired resolution in degrees
+    STEP_DELAY = 0.0005
+    GEAR_RATIO = 3.7142857
+    STEPPER_RESOLUTION = 200
+    STEP_ANGLE = 360 / STEPPER_RESOLUTION       # 1.8
 
     # initialize stepper
-    stepper = A4988(dir_pin, step_pin, ms_pins, delay=delay, step_angle=step_angle, microsteps=ms, gear_ratio=gear_ratio)
+    stepper = A4988(DIR_PIN, STEP_PIN, MS_PINS, delay=STEP_DELAY, step_angle=STEP_ANGLE, microsteps=MICROSTEPS, gear_ratio=GEAR_RATIO)
 
-    steps = int(ms360 * target_res / 360)       # 16
-    h_res = 360 * steps / ms360                 # 0.48464451
-    scan_delay = 1 / (4500 * target_res / 360)  # 0.16
+    steps = int(MS360 * TARGET_RES / 360)       # 16
+    h_res = 360 * steps / MS360                 # 0.48464451
+    scan_delay = 1 / (4500 * TARGET_RES / 360)  # 0.16
 
     # test: 360°
-    stepper.move_steps(ms360)                   # 360°
+    stepper.move_steps(MS360)                   # 360°
 
     try:
-        # 0-180° scanning
+        # 0-180° SCAN
         for z_angle in np.arange(0, 180, h_res):
             stepper.move_steps(steps)
-            time.sleep(scan_delay)              # duration of one lidar duration
+            sleep(scan_delay)                   # duration of one lidar duration
         stepper.move_steps(steps)               # n+1 step
 
-        time.sleep(1)
+        sleep(1)
 
-        # 180-360° only shooting photos
+        # 180-360° JUST SHOOTING PHOTOS
         for i in range(4):
             stepper.move_angle(45)
-            time.sleep(1)                       # delay for photo
+            sleep(1)                            # delay for photo
 
     finally:
         stepper.close()
