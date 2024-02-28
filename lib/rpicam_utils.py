a030192ab8796f367070a6ba91de52f15edcb893
LaserBorg <mail@philipgutjahr.de>
2024-02-28 20:49:32 +0100

    awb fix

--- lib/rpicam_utils.py
+++ lib/rpicam_utils.py
@@ -16,12 +16,13 @@ def take_photo(path=None,
                dims=(4056,3040), 
                exposure_time=None, 
                gain=None,
-               denoise="cdn_hq", # cdn_hq, cdn_off, cdn_fast || "off", "cdn", "luma", "average", "auto" ?
-               awb="auto",       # string for modes or tuple for manual gains (red, blue)
+               denoise="cdn_hq", # cdn_hq, cdn_off, cdn_fast
+               awbgains=None,
+               awb="auto",
                sharpness=0.5,
                saturation=0.7,
                save_raw=False, 
-               blocking=False):
+               blocking=True):
     
 
     # default path from timestamp
@@ -33,7 +34,6 @@ def take_photo(path=None,
         "--nopreview":  "",
         "--output":     path,
         "--denoise":    denoise,
-        "--awb":        awb,
         "--width":      dims[0],
         "--height":     dims[1],
         "--sharpness":  sharpness,
@@ -42,19 +42,19 @@ def take_photo(path=None,
         "--rotation":   0}  # or 180°
 
 
-    if awb.lower() in ["auto", "incadescent", "tungsten", "fluorescent", "indoor", "daylight", "cloudy"]:
+    if awbgains:
+        awbgains = ','.join(map(str, awbgains))  # convert to string and add to params
+        params["--awbgains"]  = awbgains  
+    elif awb.lower() in ["auto", "incadescent", "tungsten", "fluorescent", "indoor", "daylight", "cloudy"]:
         params["--awb"]  = awb
-    elif isinstance(awb, tuple):  # manual mode (red, blue gains)
-        params["--awbgains"]  = ','.join(map(str, awb))  # convert to string and add to params
     else:
-        raise ValueError("Invalid white balance mode or manual gains.")
+        raise ValueError("Invalid white balance mode or red/blue gains.")
 
     if exposure_time:
-        params["--shutter"] = exposure_time * 1000000  # to microseconds
+        params["--shutter"] = int(exposure_time * 1000000)  # to microseconds
 
     if gain:
-        params["--analoggain"] = gain  # == "--gain"
-        params["--digitalgain"] = 1
+        params["--gain"] = gain  # "--analoggain", "--digitalgain" ?
 
     if save_raw:
         params["--raw"] = ""
@@ -69,9 +69,10 @@ def take_photo(path=None,
     else:
         retval = subprocess.Popen(cmd_string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # check returncode if the command was successful
-    if not blocking and retval.returncode != 0:
-        raise Exception(f"Command failed with return code {retval.returncode}: {retval.stderr.decode()}")
+    # # check returncode if the command was successful
+    # if not blocking and retval.returncode != 0:
+    #     error_message = retval.stderr.read().decode()
+    #     raise Exception(f"Command failed with return code {retval.returncode}: {error_message}")
 
     return path
 
@@ -94,15 +95,21 @@ class ExifReader:
                 print(f"Key:{tag}, value {self.tags[tag]}")
 
 
-def estimate_camera_parameters(threshold=0.01, max_iterations=10):
+def estimate_camera_parameters(threshold=0.1, max_iterations=10, set_gain=None, bins=32):
 
-    def __calculate_histogram__(img):
-        hist = cv2.calcHist([img], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
+    def __calculate_channel_histogram__(img, channel, bins=256):
+        hist = cv2.calcHist([img], [channel], None, [bins], [0, 256])
         cv2.normalize(hist, hist)
         return hist
 
-    preview_dims = (640, 480)
-    awbgains = (1, 1)
+    def __calculate_RGB_histogram__(img, bins=256):
+        hist_r = __calculate_channel_histogram__(img, 0, bins)
+        hist_g = __calculate_channel_histogram__(img, 1, bins)
+        hist_b = __calculate_channel_histogram__(img, 2, bins)
+        return hist_r, hist_g, hist_b
+
+    preview_dims = (320, 240)
+    awbgains = [1, 1]
     preview_denoise = "cdn_off"
 
     tmp_dir="images/tmp"
@@ -112,47 +119,67 @@ def estimate_camera_parameters(threshold=0.01, max_iterations=10):
     path_auto = take_photo(path=os.path.join(tmp_dir,"awb.jpg"),
                            dims=preview_dims, 
                            denoise=preview_denoise, 
-                           awb="auto")
+                           awb="auto", 
+                           blocking=True)
     
-    hist_auto = __calculate_histogram__(cv2.imread(path_auto))
     exif_auto = ExifReader(path_auto)
+
+    img_auto = cv2.imread(path_auto)
+    hist1_r, hist1_g, hist1_b = __calculate_RGB_histogram__(img_auto, bins=bins)
+    
+    cv2.imshow("auto", img_auto)
+    cv2.waitKey(10)
 
     for i in range(max_iterations):
         # take photo with manual exposure, gain and awbgains
-        path_awbgains = take_photo(path=os.path.join(tmp_dir, str(i)+".jpg"),
+        path_awbgains = take_photo(path=os.path.join(tmp_dir, f"img{i}.jpg"),
                                    dims=preview_dims,
                                    denoise=preview_denoise,
                                    exposure_time=exif_auto.exposure_time,
                                    gain=exif_auto.gain,
-                                   awbgains=awbgains)
+                                   awbgains=awbgains,
+                                   blocking=True)
         
-        hist_awbgains = __calculate_histogram__(cv2.imread(path_awbgains))
+        img_awbgains = cv2.imread(path_awbgains)
+        cv2.imshow("awbgains:", img_awbgains)
+        cv2.waitKey(10)
 
-        # calculate Bhattacharyya distance
-        difference = cv2.compareHist(hist_auto, hist_awbgains, cv2.HISTCMP_BHATTACHARYYA)
-        print("difference:", difference)
+        hist2_r, hist2_g, hist2_b = __calculate_RGB_histogram__(img_awbgains, bins=bins)
 
-        if difference < threshold:
+        # calculate Bhattacharyya distance for each channel
+        diff_r = cv2.compareHist(hist1_r, hist2_r, cv2.HISTCMP_BHATTACHARYYA)
+        diff_g = cv2.compareHist(hist1_g, hist2_g, cv2.HISTCMP_BHATTACHARYYA)
+        diff_b = cv2.compareHist(hist1_b, hist2_b, cv2.HISTCMP_BHATTACHARYYA)
+
+        diff = (diff_r + diff_g + diff_b) / 3
+
+        print("iteration:", i+1, "| diff:", round(diff, 2), "(", round(diff_r,2), round(diff_g,2), round(diff_b,2), ")")
+
+        if diff < threshold:
             break
         
-        # adjust awbgains values based on difference
-        awbgains = (awbgains[0] * difference, awbgains[1] * difference)
+        # TODO: adjust awbgains values based on difference
+        awbgains = (awbgains[0] + 0.2, awbgains[1] + 0.5)
 
-    return exif_auto.exposure_time, exif_auto.gain, awbgains
+    # compute new exposure time based on custom gain
+    if set_gain:
+        gain = set_gain
+        exposure_time = exif_auto.exposure_time * exif_auto.gain / set_gain
+    else:
+        gain = exif_auto.gain
+        exposure_time = exif_auto.exposure_time
+
+    return exposure_time, gain, awbgains
 
 
 if __name__ == "__main__":
-    # # get preview image in automatic mode
-    # path_auto = take_photo(dims=(640, 480), denoise="cdn_off", awb="auto")
+    # path_auto = take_photo(dims=(640, 480), denoise="cdn_off", awb="auto", blocking=True)
     # exif_auto = ExifReader(path_auto)
-    # print("[Image 1] ExposureTime:", exif_auto.exposure_time, "| Gain:", exif_auto.gain)
+    # print("ExposureTime:", exif_auto.exposure_time, "| Gain:", exif_auto.gain)
 
-    exposure_time, gain, awbgains = estimate_camera_parameters(threshold=0.01, max_iterations=10)
-    print("auto Exposure:", exposure_time, "| Gain:", gain, "| AWB Gains:", awbgains)
 
-    time.sleep(1)
+    exposure_time, gain, awbgains = estimate_camera_parameters(set_gain=1, threshold=0.05, max_iterations=10, bins=32)
+    print("[RESULT] AE:", exposure_time, "| Gain:", gain, "| AWB:", awbgains)
 
     # take HighRes image using fixed values
-    path = take_photo(exposure_time=exposure_time, gain=gain, awb=awbgains, denoise="cdn_hq")
-    exif = ExifReader(path)
-    print("fixed Exposure:", exif.exposure_time, "| Gain:", exif.gain)
+    path = take_photo(exposure_time=exposure_time, gain=gain, denoise="cdn_hq", awbgains=awbgains)
