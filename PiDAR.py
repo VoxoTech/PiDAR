e3f3946cd892fa26e2fdf5a465c7ad09a6a740a0
LaserBorg <mail@philipgutjahr.de>
2024-02-29 13:14:13 +0100

    trying to fix main loop

--- PiDAR.py
+++ PiDAR.py
@@ -3,7 +3,7 @@ import numpy as np
 from time import sleep
 
 from lib.platform_utils import allow_serial
-from lib.matplotlib_2D import plot_2D
+# from lib.matplotlib_2D import plot_2D
 from lib.lidar_driver import LD06  #, STL27L
 from lib.a4988_driver import A4988
 from lib.rpicam_utils import take_photo, estimate_camera_parameters, ExifReader
@@ -20,14 +20,19 @@ DEBUG = False
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(DEBUG)
 
+# ENABLES
+enable_camera = False
+enable_stitching = True                     # requires enable_camera
+enable_lidar = True
+
 # LiDAR DRIVER
 PORT = '/dev/ttyS0'                         # {'Windows': 'COM10', 'RaspberryPi': '/dev/ttyS0', 'Linux': '/dev/ttyUSB0'}  # dmesg | grep "tty"
-PWM_DC = 0.4                                # duty cycle: 0.23 = 10 Hz, 0.1 = 4 Hz
+PWM_DC = 0.23                               # duty cycle: 0.23 = 6.5 Hz, 0.1 = 4 Hz
 OFFSET = np.pi / 2                          # = 90°
 FORMAT = 'npy'                              # 'npy' or 'csv' or None
-DTYPE = np.float32                          # np.float64 or np.float32
+DTYPE = np.float64                          # np.float64 or np.float32
 DATA_DIR = "data"
-VIS = plot_2D()                             # plot_2D() or None
+VIS = None  # plot_2D()                             # plot_2D() or None
 OUT_LEN = 40                                # visualize after every nth batch
 
 # A4988 DRIVER
@@ -60,10 +65,13 @@ PANO_WIDTH = 3600
 set_gain = 1
 awb_thres=0.01
 
-# extract exposure time and gain from exif data, iterate through Red/Blue Gains for custom AWB
-exposure_time, gain, awbgains = estimate_camera_parameters(set_gain=set_gain, awb_thres=awb_thres)
-if DEBUG:
-    print("[RESULT] AE:", exposure_time, "| Gain:", gain, "| AWB R:", round(awbgains[0],3), "B:", round(awbgains[1],3))
+
+# calibrate camera
+if enable_camera:
+    # extract exposure time and gain from exif data, iterate through Red/Blue Gains for custom AWB
+    exposure_time, gain, awbgains = estimate_camera_parameters(set_gain=set_gain, awb_thres=awb_thres)
+    if DEBUG:
+        print("[RESULT] AE:", exposure_time, "| Gain:", gain, "| AWB R:", round(awbgains[0],3), "B:", round(awbgains[1],3))
 
 
 # ensure output directory
@@ -71,7 +79,8 @@ make_dir(DATA_DIR)
 
 
 # initialize lidar
-lidar   = LD06(port=PORT, pwm_dc = PWM_DC, visualization=VIS, offset=OFFSET,format=FORMAT, dtype=DTYPE, data_dir=DATA_DIR, out_len=OUT_LEN)
+if enable_lidar:
+    lidar   = LD06(port=PORT, pwm_dc = PWM_DC, visualization=VIS, offset=OFFSET,format=FORMAT, dtype=DTYPE, data_dir=DATA_DIR, out_len=OUT_LEN)
 
 # initialize stepper
 stepper = A4988(DIR_PIN, STEP_PIN, MS_PINS, delay=STEP_DELAY, step_angle=STEP_ANGLE, microsteps=MICROSTEPS, gear_ratio=GEAR_RATIO)
@@ -80,50 +89,52 @@ stepper = A4988(DIR_PIN, STEP_PIN, MS_PINS, delay=STEP_DELAY, step_angle=STEP_AN
 # MAIN
 try:
     # 360° SHOOTING PHOTOS
-    for i in range(IMGCOUNT):
-        stepper.move_angle(360/IMGCOUNT)
+    if enable_camera:
+        for i in range(IMGCOUNT):
+            stepper.move_angle(360/IMGCOUNT)
 
-        # take HighRes image using fixed values
-        imgpath = take_photo(exposure_time=exposure_time, gain=gain, awbgains=awbgains, denoise="cdn_hq", save_raw=raw, blocking=True)
-        imglist.append(imgpath)
-        # sleep(1)
+            # take HighRes image using fixed values
+            imgpath = take_photo(exposure_time=exposure_time, gain=gain, awbgains=awbgains, denoise="cdn_hq", save_raw=raw, blocking=True)
+            imglist.append(imgpath)
+            # sleep(1)
     
-
     # STITCHING PROCESS (NON-BLOCKING)
-    project_path = hugin_stitch(imglist, template=template_path, width=PANO_WIDTH)
-
+    if enable_stitching and enable_camera:
+        project_path = hugin_stitch(imglist, template=template_path, width=PANO_WIDTH)
 
     # 180° SCAN
-    for z_angle in np.arange(0, 180, h_res):
+    if enable_lidar:
+        for z_angle in np.arange(0, 180, h_res):
 
-        if lidar.serial_connection.is_open:
-
-            if lidar.out_i == lidar.out_len:
-                #print("Z angle:", round(z_angle, 2))
+            if lidar.serial_connection.is_open:
+                print("Z angle:", round(z_angle, 2))
                 print("Speed:", round(lidar.speed, 2))
-                
+
                 # SAVE DATA
                 if lidar.format is not None:
                     save_data(lidar.data_dir, lidar.points_2d, lidar.format)
 
-                # VISUALIZE
-                if lidar.visualization is not None:
-                    lidar.visualization.update_coordinates(lidar.points_2d)
-                lidar.out_i = 0
 
-            # READ LIDAR DATA FROM SERIAL
-            sleep(scan_delay)
-            lidar.read()
-            lidar.out_i += 1
-            
-            stepper.move_steps(steps)
+                if lidar.out_i == lidar.out_len:
+                    # VISUALIZE
+                    if lidar.visualization is not None:
+                        lidar.visualization.update_coordinates(lidar.points_2d)
+                    lidar.out_i = 0
 
-    # last step to complete 180°
-    stepper.move_steps(steps)
+                # READ LIDAR DATA FROM SERIAL
+                sleep(scan_delay)
+                lidar.read()
+                lidar.out_i += 1
+                
+                stepper.move_steps(steps)
+
+        stepper.move_steps(steps)  # last step to complete 180°
+        stepper.move_angle(180)    # complete revolution
 
 finally:
-    print("SCANNING STOPPED")
-    lidar.close()
+    print("PiDAR STOPPED")
+    if enable_lidar:
+        lidar.close()
     stepper.close()
 
     # Relay Power off
