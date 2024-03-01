rotation fixed

PiDAR.py
@@ -65,6 +65,10 @@ PANO_WIDTH = 3600
 set_gain = 1
 awb_thres=0.01
 
+packages_per_revolution = round(4500 / 12 / 10)  # 38
+hsteps = 180 / h_res
+max_packages = hsteps * packages_per_revolution
+
 
 # calibrate camera
 if enable_camera:
@@ -73,17 +77,19 @@ if enable_camera:
     if DEBUG:
         print("[RESULT] AE:", exposure_time, "| Gain:", gain, "| AWB R:", round(awbgains[0],3), "B:", round(awbgains[1],3))
 
-
 # ensure output directory
 make_dir(DATA_DIR)
+
+# initialize stepper
+stepper = A4988(DIR_PIN, STEP_PIN, MS_PINS, delay=STEP_DELAY, step_angle=STEP_ANGLE, microsteps=MICROSTEPS, gear_ratio=GEAR_RATIO)
+
+def move_steps_callback():
+    stepper.move_steps(steps)
 
 
 # initialize lidar
 if enable_lidar:
     lidar   = LD06(port=PORT, pwm_dc = PWM_DC, visualization=VIS, offset=OFFSET,format=FORMAT, dtype=DTYPE, data_dir=DATA_DIR, out_len=OUT_LEN)
-
-# initialize stepper
-stepper = A4988(DIR_PIN, STEP_PIN, MS_PINS, delay=STEP_DELAY, step_angle=STEP_ANGLE, microsteps=MICROSTEPS, gear_ratio=GEAR_RATIO)
 
 
 # MAIN
@@ -104,29 +110,28 @@ try:
 
     # 180° SCAN
     if enable_lidar:
-        for z_angle in np.arange(0, 180, h_res):
+        lidar.read_loop(callback=move_steps_callback, max_packages=max_packages)
 
-            if lidar.serial_connection.is_open:
-                print("Z angle:", round(z_angle, 2))
-                print("Speed:", round(lidar.speed, 2))
+        # for z_angle in np.arange(0, 180, h_res):
+        #     if lidar.serial_connection.is_open:
+        #         print("Z angle:", round(z_angle, 2))
+        #         print("Speed:", round(lidar.speed, 2))
 
-                # SAVE DATA
-                if lidar.format is not None:
-                    save_data(lidar.data_dir, lidar.points_2d, lidar.format)
+        #         # SAVE DATA
+        #         if lidar.format is not None:
+        #             save_data(lidar.data_dir, lidar.points_2d, lidar.format)
 
+        #         if lidar.out_i == lidar.out_len:
+        #             # VISUALIZE
+        #             if lidar.visualization is not None:
+        #                 lidar.visualization.update_coordinates(lidar.points_2d)
+        #             lidar.out_i = 0
 
-                if lidar.out_i == lidar.out_len:
-                    # VISUALIZE
-                    if lidar.visualization is not None:
-                        lidar.visualization.update_coordinates(lidar.points_2d)
-                    lidar.out_i = 0
-
-                # READ LIDAR DATA FROM SERIAL
-                sleep(scan_delay)
-                lidar.read()
-                lidar.out_i += 1
-                
-                stepper.move_steps(steps)
+        #         # READ LIDAR DATA FROM SERIAL
+        #         sleep(scan_delay)
+        #         lidar.read()
+        #         lidar.out_i += 1
+        #         stepper.move_steps(steps)
 
         stepper.move_steps(steps)  # last step to complete 180°
         stepper.move_angle(180)    # complete revolution

lib/lidar_driver.py
@@ -92,7 +92,8 @@ class LD06:
         print("Serial connection closed.")
     
 
-    def read_loop(self):
+    def read_loop(self, callback=None, max_packages=None):
+        loop_count = 0
         if self.visualization is not None:
             # matplotlib close event
             def on_close(event):
@@ -100,11 +101,14 @@ class LD06:
                 print("Closing...")
             self.visualization.fig.canvas.mpl_connect('close_event', on_close)
 
-        while self.serial_connection.is_open:
+        while self.serial_connection.is_open and (max_packages is None or loop_count <= max_packages):
             try:
                 if self.out_i == self.out_len:
-                    # print("speed:", round(self.speed, 2))
+                    #print("speed:", round(self.speed, 2))
                     
+                    if callback is not None:
+                        callback()
+                
                     # SAVE DATA
                     if self.format is not None:
                         save_data(self.data_dir, self.points_2d, self.format)
@@ -117,6 +121,7 @@ class LD06:
                 print("SerialException")
                 break
             self.out_i += 1
+            loop_count += 1
 
 
     def read(self):
@@ -183,41 +188,50 @@ class LD06:
         return x_list, y_list
 
 
-
 class STL27L(LD06):
-    def __init__(self, port=None, pwm_channel=0, pwm_dc=0.4, offset=0, data_dir="data", out_len=40, format=None, visualization=None, dtype=np.float32):
+    def __init__(self, port='COM6', pwm_channel=0, pwm_dc=0.4, offset=0, data_dir="data", out_len=200, format=None, visualization=None, dtype=np.float32):
         # call the __init__ method of the parent class with the new baudrate and sampling rate
         super().__init__(port, pwm_channel, pwm_dc, 921600, offset, data_dir, out_len, format, visualization, dtype)
         self.sampling_rate = 21600  # new sampling rate for STL27L
 
 
+def my_callback():
+    print("Callback function called!")
+
 
 if __name__ == "__main__":
-    from matplotlib_2D import plot_2D
 
-    # CONSTANTS
-    PORT = 'COM10'  # {'Windows': 'COM10', 'RaspberryPi': '/dev/ttyACM0', 'Linux': '/dev/ttyUSB0'}[platform.system()] 
-    ANGLE_OFFSET = np.pi / 2    # = 90°
-    FORMAT = 'npy'              # 'npy' or 'csv' or None
-    DTYPE = np.float64
-    DATA_DIR = "data"
-    VISUALIZATION = plot_2D()   # plot_2D() or None
-    OUT_LEN = 40                # visualize after every nth batch
+    visualize = True
 
-    # ensure output directory
-    if not os.path.exists(DATA_DIR):
-        os.makedirs(DATA_DIR)
+    if visualize:
+        from matplotlib_2D import plot_2D
+        visualization = plot_2D()
+    else:
+        import threading
+        visualization = None
+    
+    # 4500 points per second / 12 points per batch / 10 revolutions per second = 37.5 batches per revolution
+    packages_per_revolution = 38
+    hsteps = 50
+    max_packages = hsteps * packages_per_revolution
 
-    lidar = LD06(port=PORT,
-                visualization=VISUALIZATION,
-                offset=ANGLE_OFFSET, 
-                format=FORMAT,
-                dtype=DTYPE,
-                data_dir=DATA_DIR,
-                out_len=OUT_LEN)
+
+    lidar = LD06(port = 'COM8',
+                 offset = np.pi / 2, 
+                 format = 'npy',
+                 dtype =np.float64,
+                 data_dir ="data",
+                 visualization = visualization,
+                 out_len = packages_per_revolution)  
 
     try:
         if lidar.serial_connection.is_open:
-            lidar.read_loop()
+
+            if visualize:
+                lidar.read_loop(callback=my_callback, max_packages=max_packages)
+            else:
+                read_thread = threading.Thread(target=lidar.read_loop, kwargs={'callback': my_callback, 'max_packages': max_packages})
+                read_thread.start()
+                read_thread.join()
     finally:
         lidar.close()

lib/pointcloud.py
@@ -63,12 +63,14 @@ def import_pointcloud(filepath, columns="XYZI", csv_delimiter=",", as_array=Fals
     return pcd
 
 # export point cloud to file (pcd, ply, e57, csv)
-def export_pointcloud(pcd, savepath, type="pcd", write_ascii=True, compressed=True):
-    if type == "pcd" or type == "ply":
-        if write_ascii:
-            compressed = False
-        o3d.io.write_point_cloud(savepath+"."+type, pcd, write_ascii=write_ascii, compressed=compressed),
+def export_pointcloud(pcd, savepath, type="pcd"):
+    if type == "pcd":
+        o3d.io.write_point_cloud(savepath+"."+type, pcd)
+    
+    elif type == "ply":
+        o3d.io.write_point_cloud(savepath+"."+type, pcd, write_ascii=True, compressed=False)
 
+    # TODO: add support for exporting intensity and RGB colors
     elif type == "csv":
         if not isinstance(pcd, np.ndarray):
             array = np.asarray(pcd.points)
@@ -76,10 +78,10 @@ def export_pointcloud(pcd, savepath, type="pcd", write_ascii=True, compressed=Tr
 
     elif type == "e57":
         # if a single point cloud is provided, convert it to a list
-        if isinstance(pcd, o3d.geometry.PointCloud):
-            pcd_list = [pcd]
-        elif isinstance(pcd, list): 
+        if isinstance(pcd, list): 
             pcd_list = pcd
+        elif isinstance(pcd, o3d.geometry.PointCloud):
+            pcd_list = [pcd]
 
         # Create an E57 object with write mode
         e57 = pye57.E57(savepath+"."+type, mode='w')
@@ -102,6 +104,10 @@ def export_pointcloud(pcd, savepath, type="pcd", write_ascii=True, compressed=Tr
             # Write the point cloud data to the E57 file
             e57.write_scan_raw(data_raw)
         e57.close()
+
+# Remove rows with NaN values from a numpy array
+def remove_NaN(array):
+    return array[~np.isnan(array).any(axis=1)]
 
 # estimate normals for a point cloud
 def estimate_point_normals(pcd, radius=0.1, max_nn=30):
@@ -145,7 +151,11 @@ def pcd_from_np(array, columns="XYZI"):
 # load list of point cloud files and merge them by angle.
 # returns pcd or numpy array
 def merge_data(filepaths, angle_step=0, offset=(0,0,0), up_axis="Z", columns="XZI", csv_delimiter=",", as_pcd=True):
-    if up_axis.upper() == "Z":
+    if up_axis.upper() == "X":
+        rotation_axis = np.array([1, 0, 0])
+    elif up_axis.upper() == "Y":
+        rotation_axis = np.array([0, 1, 0])
+    else:  # up_axis.upper() == "Z":
         rotation_axis = np.array([0, 0, 1])
 
     # init result object with (X,Y,Z, intensity)
@@ -158,9 +168,12 @@ def merge_data(filepaths, angle_step=0, offset=(0,0,0), up_axis="Z", columns="XZ
         # insert 3D Y=0 after column 0 so 2D-Y becomes 3D-Z (Z-up: image is now vertical)
         points3d = np.insert(points2d, 1, values=0, axis=1)
 
-        points3d = rotate_3D(points3d, angle, translation_vector=offset, rotation_axis=(0,0,1))
+        points3d = rotate_3D(points3d, angle, translation_vector=offset, rotation_axis=rotation_axis)
         pointcloud = np.append(pointcloud, points3d, axis=0)
         angle += angle_step
+    
+    # Remove rows with NaN values
+    pointcloud = remove_NaN(pointcloud)
 
     if as_pcd:
         return pcd_from_np(pointcloud)
@@ -269,8 +282,8 @@ if __name__ == "__main__":
 
     opengl_fallback()
 
-    filepaths = list_files("data", type='npy')
-    pcd = merge_data(filepaths, angle_step=5.5, offset=(0, 10, 0), up_axis="Z", columns="XZI", as_pcd=True)
+    filepaths = list_files("data/scan1", type='npy')
+    pcd = merge_data(filepaths, angle_step=0.25, offset=(0, 2, -4), up_axis="Z", columns="XZI", as_pcd=True)
 
-    export_pointcloud([pcd], "export/3d-scan", type="e57")
+    export_pointcloud(pcd, "export/3d-scan", type="e57")
     visualize([pcd], unlit=True)